7263191db1781a1badb0d8d4891c1c22b496661c
alisaivi786 <alisaivi786@gmail.com>
2024-06-05 23:17:43 +0400

    Add Layout and NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { Button } from "@chakra-ui/react"
+import { Grid, GridItem, Show } from "@chakra-ui/react"
+import NavBar from "./components/NavBar"
+
 
 function App() {
     return (
-    <>
-    <Button colorScheme='blue'>Button</Button>
-    </>
+      <Grid templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`
+  
+      }}>
+        <GridItem area="nav">
+          <NavBar />
+        </GridItem>
+        <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Sidebar
+        </GridItem>
+        </Show>
+        <GridItem area="main" bg="blue">
+          Main
+        </GridItem>
+      </Grid>
   )
 }
 
